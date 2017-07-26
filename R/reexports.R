d14bd677e9c3ca32affc5652b345859e563114ac
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-26 19:46:15 -0400

    Initial commit of offloaded devtools from basejump

---
+++ R/reexports.R
@@ -0,0 +1,45 @@
+# BiocInstaller ====
+#' @export
+BiocInstaller::biocValid
+
+
+
+# devtools ====
+#' @export
+devtools::build
+
+#' @export
+devtools::build_vignettes
+
+#' @export
+devtools::document
+
+#' @export
+devtools::install
+
+#' @export
+devtools::load_all
+
+#' @export
+devtools::test
+
+#' @export
+devtools::use_data
+
+#' @export
+devtools::use_data_raw
+
+#' @export
+devtools::use_testthat
+
+
+
+# lintr ====
+#' @export
+lintr::lint_package
+
+
+
+# magrittr ====
+#' @export
+magrittr::`%>%`
