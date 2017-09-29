ecc33e440c360dc4cf70d59ee5783332a5509c30
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-29 16:07:22 -0400

    Simplify loading

--- R/mjs-package.R
+++ R/mjs-package.R
@@ -5,13 +5,7 @@
 #' @import devtools
 #'
 #' @importFrom BiocCheck BiocCheck
-#' @importFrom BiocInstaller biocValid
-#' @importFrom covr package_coverage
-#' @importFrom lintr lint_package
-#' @importFrom magrittr %>%
-#' @importFrom pkgdown build_site
 #' @importFrom rmarkdown render
-#' @importFrom testthat expect_equal
 #' @importFrom tools file_path_sans_ext
 #' @importFrom utils globalVariables
 "_PACKAGE"
