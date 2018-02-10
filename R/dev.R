c6950c87ee2dd8e8e0315b561e03627be601da27
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-10 07:41:41 -0500

    Add assertive and improve man `dev()` loading call

--- R/dev.R
+++ R/dev.R
@@ -1,56 +1,49 @@
 #' Load Developer Environment
 #'
 #' @importFrom BiocInstaller biocValid
+#' @importFrom desc desc_get_deps
 #' @importFrom utils installed.packages
 #'
-#' @return No return.
+#' @return Invisible character vector of packages attached specifically by
+#'   this function call.
 #' @export
 dev <- function() {
-    # Order is important here
-    packages <-
-        c("jsonlite",
-          "magrittr",
-          "Matrix",
-          "pbapply",
-          "scales",
-          "viridis",
-          # Package development ================================================
-          "devtools",
-          "covr",
-          "lintr",
-          "testthat",
-          # RStudio core =======================================================
-          "googlesheets",
-          "knitr",
-          "pkgdown",
-          "readxl",
-          "rmarkdown",
-          "stringr",
-          # Final NAMESPACE bootup =============================================
-          "Biobase",
-          "BiocInstaller",
-          "S4Vectors",
-          "basejump",
-          "tidyverse",
-          "rlang")
+    deps <- desc_get_deps(path.package("bb8"))
+    packages <- deps %>%
+        .[.[["type"]] != "Depends", ] %>%
+        .[["package"]]
+
+    # Order of final packages to load is important
+    final <- c(
+        "Biobase",
+        "BiocInstaller",
+        "S4Vectors",
+        "basejump",
+        "rlang",
+        "tidyverse"
+    )
+
+    packages <- c(setdiff(packages, final), final)
 
     # Stop on missing packages
-    notInstalled = setdiff(packages, rownames(installed.packages()))
+    notInstalled <- setdiff(packages, rownames(installed.packages()))
     if (length(notInstalled) > 0) {
-        stop(paste(
-            "Not installed:",
-            toString(notInstalled)
-        ), call. = FALSE)
+        stop(paste("Not installed:", toString(notInstalled)), call. = FALSE)
     }
 
     # Attach unloaded packages
-    lapply(seq_along(packages), function(a) {
-        if (!packages[[a]] %in% (.packages())) {
-            attachNamespace(packages[[a]])
-        }
-    })
-    invisible()
+    attached <- lapply(
+        X = packages,
+        FUN = function(package) {
+            if (!package %in% (.packages())) {
+                attachNamespace(package)
+                package
+            }
+        })
+    attached <- unlist(attached)
 
     # Check that workspace is kosher
     biocValid(silent = TRUE)
+
+    invisible(attached)
 }
