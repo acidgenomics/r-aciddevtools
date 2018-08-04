deb511987473f7b0ae751afb2949ba1ae040fe3e
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-04 06:09:18 -0400

    Add `catVec()` function

---
+++ R/catVec.R
@@ -0,0 +1,14 @@
+#' Concatenate and Print a Vector
+#'
+#' Print vector to console quoted, with comma delims
+#'
+#' @return Console output.
+#' @export
+#'
+#' @examples
+#' c("hello", "world") %>% catVec()
+catVec <- function(x) {
+    x <- paste0("\"", x, "\"")
+    x <- paste(x, collapse = ",\n")
+    cat(x)
+}
