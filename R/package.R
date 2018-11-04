b29536e4c581e4dc39a812b1f735c787c805e36f
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-04 18:52:08 -0500

    Simplify package

--- R/package.R
+++ R/package.R
@@ -2,16 +2,8 @@
 #'
 #' Trusty sidekick for R package development.
 #'
-#' @import methods
-#' @importFrom BiocManager install valid
-#' @importFrom assertthat assert_that
+#' @importFrom BiocManager valid
 #' @importFrom desc desc_get_deps
-#' @importFrom devtools update_packages
 #' @importFrom tidyverse tidyverse_conflicts
-#' @importFrom rmarkdown render
-#' @importFrom utils globalVariables installed.packages
+#' @importFrom utils installed.packages
 "_PACKAGE"
-
-
-
-globalVariables(".")
