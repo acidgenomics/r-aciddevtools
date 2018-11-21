42ba80d5d914846fed8929c9c22eb75a7e663ef1
Michael Steinbaugh <mike@steinbaugh.com>
2018-11-21 16:34:41 +0000

    Simplify the package to only import desc and utils

--- R/package.R
+++ R/package.R
@@ -1,7 +1,5 @@
 #' @importFrom BiocManager valid
 #' @importFrom desc desc_get_deps
-#' @importFrom magrittr %>%
-#' @importFrom tidyverse tidyverse_conflicts
 #' @importFrom utils globalVariables installed.packages
 NULL
 
