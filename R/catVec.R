dfd6cd208afda641c50f8f45a3f8d5b305d6de6c
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-12 11:48:43 -0400

    Migrate functions from Rprofile

--- R/catVec.R
+++ R/catVec.R
@@ -1,6 +1,10 @@
 #' Print (cat) a vector
+#'
 #' @export
+#'
 #' @param x Object.
+#'
+#' @return Console output.
 catVec <- function(x) {
     x <- paste0("\"", x, "\"")
     x <- paste(x, collapse = ",\n")
