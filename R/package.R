c0c8f6ee313adada613297feebd06cddd347cc87
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-12 06:54:46 -0400

    Draft updates

--- R/package.R
+++ R/package.R
@@ -1,5 +1,13 @@
+#' bb8
+#' Trusty sidekick for R package development
+#' Personal package with additional devtools utilities.
+#'
+#' @importFrom basejump initDir
 #' @importFrom desc desc_get_deps
+#' @importFrom goalie assert isAny isString isSubset
+#' @importFrom methods is
+#' @importFrom tools Rd_db
 #' @importFrom utils globalVariables installed.packages
-NULL
+"_PACKAGE"
 
 globalVariables(".")
