591e14933e65201e0e8ac13f0ca5a4baa164265b
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-23 06:53:10 -0400

    Improve kill functions

--- R/kill.R
+++ R/kill.R
@@ -1,19 +1,37 @@
-#' Kill current R session
+#' Kill R session
 #'
-#' @export
-#' @note Updated 2019-10-22.
+#' @name kill
+#' @note Updated 2019-10-23.
+#'
+#' @param pid `integer(1)`.
+#'   Process ID.
 #'
 #' @seealso
 #' - RStudio: Session > Restart R (Ctrl + Shift + F10)
 #' - https://github.com/vsbuffalo/dotfiles/blob/master/.Rprofile
 #'
 #' Useful kill commands in shell:
-#' - `kill -WINCH <PID>`.
-#' - `pkill rsession`.
 #' - `pgrep rsession`.
+#' - `pkill rsession`.
+#' - `kill -WINCH <PID>`.
 #'
 #' @examples
 #' ## > kill()
-kill <- function() {
-    system2(command = "kill", args = Sys.getpid())
+#' ## > killAll()
+NULL
+
+
+
+#' @rdname kill
+#' @export
+kill <- function(pid = Sys.getpid()) {
+    system2(command = "kill", args = pid)
+}
+
+
+
+#' @rdname killAll
+#' @export
+killAll <- function() {
+    system2(command = "pkill", args = "rsession")
 }
