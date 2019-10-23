f9617e4cda67540cfa223a0ccfebe3064f5db847
Michael Steinbaugh <mike@steinbaugh.com>
2019-10-23 07:00:52 -0400

    Use system2 instead of system

--- R/finder.R
+++ R/finder.R
@@ -2,7 +2,7 @@
 #'
 #' @export
 #' @note macOS only.
-#' @note Updated 2019-08-13.
+#' @note Updated 2019-10-23.
 #'
 #' @inheritParams params
 #'
@@ -12,5 +12,5 @@ finder <- function(path = ".") {
         Sys.info()[[1L]] == "Darwin",
         is.character(path) && identical(length(character), 1L)
     )
-    system(paste("open", path))
+    system2(command = "open", args = path)
 }
