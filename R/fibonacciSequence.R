899861bb236cacc4e39a31eddd4a1749805239cf
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-09 14:47:06 -0400

    Improve fibonacci sequence code

--- R/fibonacciSequence.R
+++ R/fibonacciSequence.R
@@ -1,7 +1,7 @@
 #' Fibonacci sequence
 #'
 #' @export
-#' @note Updated 2019-07-26.
+#' @note Updated 2020-04-09.
 #'
 #' @param len `integer(1)`.
 #'
@@ -10,10 +10,13 @@
 #' @examples
 #' fibonacciSequence(8L)
 fibonacciSequence <- function(len) {
-    stopifnot(is.integer(len), identical(length(len), 1L))
+    stopifnot(isInt(len))
+    ## Set the lengh of the numeric vector.
     x <- numeric(len)
     x[1L] <- 1L  # nolint
+    if (identical(length(x), 1L)) return(x)
     x[2L] <- 1L  # nolint
+    if (identical(length(x), 2L)) return(x)
     for (i in 3L:len) {
         x[i] <- x[i - 1L] + x[i - 2L]
     }
