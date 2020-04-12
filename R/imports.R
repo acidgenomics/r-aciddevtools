222177db2704870e71eb5f754858529a2a349a25
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-12 09:44:33 -0400

    Prepare release

--- R/imports.R
+++ R/imports.R
@@ -3,6 +3,6 @@
 #'   isADir isAFile isAny isCharacter isFlag isInstalled isInt isLinux isMacOS
 #'   isRStudio isString
 #' @importFrom methods is
-#' @importFrom utils View capture.output install.packages installed.packages
-#'   packageDescription remove.packages
+#' @importFrom utils View capture.output download.file install.packages
+#'   installed.packages packageDescription remove.packages untar
 NULL
