e5a82c3b45b33a7fcefd56ac145640500a2b9457
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-12 09:06:57 -0400

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,8 @@
 #' @importFrom acidbase pasteURL requireNamespaces shell
-#' @importFrom goalie assert hasLength isInstalled isInt isLinux isMacOS
+#' @importFrom goalie assert allAreMatchingRegex areSameLength hasLength
+#'   isADir isAFile isAny isCharacter isFlag isInstalled isInt isLinux isMacOS
+#'   isRStudio isString
+#' @importFrom methods is
+#' @importFrom utils View capture.output install.packages installed.packages
+#'   packageDescription remove.packages
 NULL
