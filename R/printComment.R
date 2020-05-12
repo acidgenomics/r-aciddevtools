5e7070f713630b2a6fd081cd86a2b57650e397be
Michael Steinbaugh <mike@steinbaugh.com>
2020-05-12 14:20:11 -0400

    Prepare release

--- R/printComment.R
+++ R/printComment.R
@@ -1,11 +1,14 @@
 #' Print as comment
 #'
 #' @export
-#' @note Updated 2020-04-12.
+#' @note Updated 2020-05-12.
 #'
 #' @param ... Passthrough to `print()`.
 #' @param prefix Comment prefix to use. RStudio, roxygen, and ESS prefixes
 #'   are supported.
+#' @param `integer(1L)`.
+#'   Desired output width.
+#'   Defaults to 80 characters.
 #'
 #' @return Console output.
 printComment <- function(
@@ -17,11 +20,17 @@ printComment <- function(
         "#' >",
         "#",
         "# >"
-    )
+    ),
+    width = 80L
 ) {
     stopifnot(requireNamespace("utils", quietly = TRUE))
     prefix <- match.arg(prefix)
+    ## Always set width at 80 characters.
+    defaultWidth <- getOption("width")
+    options(width = width)
     out <- utils::capture.output(print(...))
     out <- paste(prefix, out)
     cat(out, sep = "\n")
+    ## Reset the width back to default.
+    options(width = defaultWidth)
 }
