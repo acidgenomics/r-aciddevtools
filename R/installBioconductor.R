17e62b29755fa523d6572761244dbced4c307189
Michael Steinbaugh <mike@steinbaugh.com>
2020-11-03 08:50:59 -0500

    Switch to only using base R code

---
+++ R/installBioconductor.R
@@ -0,0 +1,43 @@
+#' Install Bioconductor
+#'
+#' @details
+#' - Installs BiocManager package automatically, if necessary.
+#' - Calls [BiocManager::install()] internally.
+#'
+#' @export
+#' @note Updated 2020-11-03.
+#'
+#' @param version `character(1)`.
+#'   Bioconductor release version.
+#'   If left unset, defaults to latest release.
+#'
+#' @return Status from [BiocManager::install()].
+#'
+#' @seealso [BiocManager::version()].
+#'
+#' @examples
+#' ## > installBioconductor()
+installBioconductor <- function(
+    version = Sys.getenv("BIOC_VERSION")
+) {
+    version <- as.character(version)
+    if (isFALSE(nzchar(version))) {
+        version <- character()
+    }
+    message(sprintf(
+        "Installing Bioconductor %s.",
+        ifelse(
+            test = isTRUE(length(version) == 1L),
+            yes = version,
+            no = "latest release"
+        )
+    ))
+    if (!requireNamespace("BiocManager", quietly = TRUE)) {
+        install.packages("BiocManager")
+    }
+    stopifnot(requireNamespace("BiocManager", quietly = TRUE))
+    if (!isTRUE(length(version) == 1L)) {
+        version <- BiocManager::version()
+    }
+    BiocManager::install(update = FALSE, ask = FALSE, version = version)
+}
