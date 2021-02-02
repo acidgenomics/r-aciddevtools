af559780b23f60de518245e111a911f9b1142670
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-02 16:42:42 -0500

    Draft update to aliases

---
+++ R/aliases.R
@@ -0,0 +1,23 @@
+#' Aliases
+#'
+#' Shortcut aliases to reduce common keystrokes.
+#'
+#' @name aliases
+#' @note Updated 2021-02-02.
+NULL
+
+
+
+#' @rdname aliases
+#' @export
+d <- function(...) {
+    document(...)
+}
+
+
+
+#' @rdname aliases
+#' @export
+la <- function(...) {
+    load_all(...)
+}
