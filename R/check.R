6144eaa62bc2478921bab239cfc7dc40b004655c
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-04 12:32:03 -0400

    Check coverage automatically by default

--- R/check.R
+++ R/check.R
@@ -12,7 +12,12 @@
 #'
 #' @examples
 #' ## > check()
-check <- function(path = ".", cran = FALSE) {
+check <- function(
+    path = ".",
+    cran = FALSE,
+    biocCheck = TRUE,
+    coverage = TRUE
+) {
     stopifnot(
         requireNamespace("desc", quietly = TRUE),
         requireNamespace("rcmdcheck", quietly = TRUE),
@@ -43,18 +48,52 @@ check <- function(path = ".", cran = FALSE) {
     }
     message("Running package checks with 'rcmdcheck()'.")
     rcmdcheck(path = path, cran = cran)
-    ## Only run BiocCheck if we detect "biocViews" in DESCRIPTION and when the
-    ## directory name is identical to the package name. BiocCheck currently
-    ## errors on directory names that differ from the package name.
-    ok <- all(
-        !is.na(keys[["biocViews"]]),
-        identical(pkgName, basename(path)),
-        .isInstalled("BiocCheck")
-    )
-    if (isTRUE(ok)) {
-        message("Running additional Bioconductor checks with 'BiocCheck()'.")
-        stopifnot(requireNamespace("BiocCheck", quietly = TRUE))
-        BiocCheck(package = path)
+    if (isTRUE(biocCheck)) {
+        ## Only run BiocCheck if we detect "biocViews" in DESCRIPTION and when
+        ## the directory name is identical to the package name. BiocCheck
+        ## currently errors on directory names that differ from package name.
+        ok <- all(
+            !is.na(keys[["biocViews"]]),
+            identical(pkgName, basename(path)),
+            .isInstalled("BiocCheck")
+        )
+        if (isTRUE(ok)) {
+            message("Running Bioconductor checks with 'BiocCheck()'.")
+            BiocCheck(package = path)
+        }
+    }
+    if (isTRUE(coverage)) {
+        .checkCoverage(path = path)
+    }
+    invisible(TRUE)
+}
+
+
+
+#' Check package coverage using covr
+#'
+#' @note Updated 2021-06-04.
+#' @noRd
+.checkCoverage <- function(
+    path = ".",
+    cutoff = 0.80
+) {
+    if (!dir.exists("tests")) {
+        message("No unit tests defined in 'tests' directory.")
+        return(invisible(FALSE))
+    }
+    message("Checking coverage with covr package.")
+    pctCutoff <- cutoff * 100L
+    if (isTRUE(nzchar(Sys.getenv("RCHECK_KEEP_IT_100")))) {
+        ## covr sometimes reports 99.8% coverage instead of 100%.
+        pctCutoff <- 99L
+        message("Requiring 100% coverage.")
+    }
+    cov <- package_coverage(path = path)
+    pct <- percent_coverage(cov)
+    print(cov)
+    if (pct < pctCutoff) {
+        stop(sprintf("Coverage is %s%%.", round(pct, digits = 2L)))
     }
     invisible(TRUE)
 }
