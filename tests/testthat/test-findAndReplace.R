8d5189815849fef5b46837733f4b2e57924ea98a
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-23 09:38:47 -0400

    Add code coverage

--- tests/testthat/test-findAndReplace.R
+++ tests/testthat/test-findAndReplace.R
@@ -1,8 +1,59 @@
 context("findAndReplace")
 
 test_that("Non-recursive", {
-
+    unlink("testdata", recursive = TRUE)
+    dir.create(file.path("testdata", "subdir"), recursive = TRUE)
+    writeLines(
+        text = "print(\"foo\")",
+        con = file.path("testdata", "aaa.R"),
+    )
+    writeLines(
+        text = "print(\"foo\")",
+        con = file.path("testdata", "subdir", "bbb.R"),
+    )
+    out <- findAndReplace(
+        pattern = "foo",
+        replacement = "bar",
+        dir = "testdata",
+        recursive = FALSE
+    )
+    expect_identical(
+        object = basename(out),
+        expected = "aaa.R"
+    )
+    expect_identical(
+        object = readLines(out[[1L]]),
+        expected = "print(\"bar\")"
+    )
+    unlink("testdata", recursive = TRUE)
 })
 
 test_that("Recursive", {
+    unlink("testdata", recursive = TRUE)
+    dir.create(file.path("testdata", "subdir"), recursive = TRUE)
+    writeLines(
+        text = "print(\"foo\")",
+        con = file.path("testdata", "aaa.R"),
+    )
+    writeLines(
+        text = "print(\"foo\")",
+        con = file.path("testdata", "subdir", "bbb.R"),
+    )
+    out <- findAndReplace(
+        pattern = "foo",
+        replacement = "bar",
+        dir = "testdata",
+        recursive = TRUE
+    )
+    expect_identical(
+        object = basename(out),
+        expected = c("aaa.R", "bbb.R")
+    )
+    for (file in out) {
+        expect_identical(
+            object = readLines(file),
+            expected = "print(\"bar\")"
+        )
+    }
+    unlink("testdata", recursive = TRUE)
 })
