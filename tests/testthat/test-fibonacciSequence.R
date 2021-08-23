162e8bcab60c0194faa1a4742e68d282fe5c2e89
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-23 10:29:49 -0400

    Work toward 100% coverage

--- tests/testthat/test-fibonacciSequence.R
+++ tests/testthat/test-fibonacciSequence.R
@@ -2,6 +2,14 @@ context("fibonacciSequence")
 
 test_that("fibonacciSequence", {
     expect_identical(
+        object = fibonacciSequence(1L),
+        expected = 1L
+    )
+    expect_identical(
+        object = fibonacciSequence(2L),
+        expected = c(1L, 2L)
+    )
+    expect_identical(
         object = fibonacciSequence(8L),
         expected = c(1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L)
     )
