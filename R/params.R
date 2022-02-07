03730c9fdf0bfe2dbd0a854e4ef3b221c9250b9a
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-07 13:03:24 -0500

    Reorganize params documentation

---
+++ R/params.R
@@ -0,0 +1,24 @@
+#' Parameters
+#'
+#' @name params
+#' @keywords internal
+#'
+#' @param coverage `logical(1)`.
+#'   Check that unit tests provide sufficient coverage.
+#' @param dir `character(1)`.
+#'   Directory path.
+#' @param lib `character`.
+#'   Destination library directory path.
+#'   Defaults to the first element of `.libPaths()`.
+#' @param path `character(1)`.
+#'   File path (e.g. to package).
+#' @param pkgs `character`.
+#'   Package names.
+#' @param object Object.
+#' @param reinstall `logical(1)`.
+#'   Force reinstallation of any existing packages.
+#' @param x Object.
+#' @param ... Additional arguments.
+#'
+#' @return No value.
+NULL
