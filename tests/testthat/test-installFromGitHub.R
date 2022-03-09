ea5064627d0ac155c2853e2367372d8cc3fb73bf
Michael Steinbaugh <mike@steinbaugh.com>
2022-03-09 12:17:38 -0500

    Add branch support for installFromGitHub

---
+++ tests/testthat/test-installFromGitHub.R
@@ -0,0 +1,63 @@
+context("installFromGitHub")
+
+testlib <- file.path(tempdir(), "testlib")
+
+test_that("tag", {
+    unlink(testlib, recursive = TRUE)
+    pkgName <- "goalie"
+    repo <- paste0("acidgenomics/r-", pkgName)
+    tag <- "v0.5.4"
+    installFromGitHub(
+        repo = repo,
+        tag = tag,
+        lib = testlib,
+        reinstall = TRUE
+    )
+    expect_true(all(
+        pkgName %in% list.dirs(
+            path = testlib,
+            full.names = FALSE,
+            recursive = FALSE
+        )
+    ))
+    expect_message(
+        object = installFromGitHub(
+            repo = repo,
+            tag = tag,
+            lib = testlib,
+            reinstall = FALSE
+        ),
+        regexp = "is installed"
+    )
+    unlink(testlib, recursive = TRUE)
+})
+
+test_that("branch", {
+    unlink(testlib, recursive = TRUE)
+    pkgName <- "goalie"
+    repo <- paste0("acidgenomics/r-", pkgName)
+    branch <- "main"
+    installFromGitHub(
+        repo = repo,
+        branch = branch,
+        lib = testlib,
+        reinstall = TRUE
+    )
+    expect_true(all(
+        pkgName %in% list.dirs(
+            path = testlib,
+            full.names = FALSE,
+            recursive = FALSE
+        )
+    ))
+    expect_message(
+        object = installFromGitHub(
+            repo = repo,
+            branch = branch,
+            lib = testlib,
+            reinstall = FALSE
+        ),
+        regexp = "is installed"
+    )
+    unlink(testlib, recursive = TRUE)
+})
