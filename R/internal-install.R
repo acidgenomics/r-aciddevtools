4ab7c036239e34fc1052a898949612d4f1ac84e0
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-28 10:13:43 -0400

    Improve our Makevars compiler overrides

--- R/internal-install.R
+++ R/internal-install.R
@@ -2,40 +2,7 @@
 
 
 
-#' Install packages, if necessary
-#'
-#' @note Updated 2021-08-22.
-#' @noRd
-#'
-#' @param pkgs `character`.
-#' R package names to install.
-#'
-#' @param lib `character(1)`.
-#' R package library path.
-#' See `.libPaths()` for details.
-#'
-#' @return Invisible `logical(1)`
-#'
-#' @examples
-#' ## > .installIfNecessary("BiocManager")
-.installIfNecessary <- function(pkgs, lib = .libPaths()[[1L]]) {
-    warn <- getOption(x = "warn")
-    options("warn" = 2L)
-    invisible(lapply(
-        X = pkgs,
-        FUN = function(pkg) {
-            if (!requireNamespace(pkg, quietly = TRUE)) {
-                utils::install.packages(
-                    pkgs = pkg,
-                    repos = .cran,
-                    lib = lib
-                )
-            }
-        }
-    ))
-    options("warn" = warn)
-    invisible(TRUE)
-}
+
 
 
 
