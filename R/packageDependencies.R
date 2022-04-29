e543023b8209081044121a3d9601d3fbd130c902
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-29 10:42:32 -0400

    Add dependency checker

---
+++ R/packageDependencies.R
@@ -0,0 +1,34 @@
+#' Package dependencies
+#'
+#' @export
+#' @note Updated 2022-04-29.
+#'
+#' @param pkg `character(1)`.
+#' Package name.
+#'
+#' @return `character`.
+#' Names of dependency packages.
+#'
+#' @examples
+#' packageDependencies("stats")
+packageDependencies <- function(pkg) {
+    stopifnot(
+        requireNamespace("BiocManager", quietly = TRUE),
+        requireNamespace("tools", quietly = TRUE),
+        is.character(pkg) && identical(length(pkg), 1L)
+    )
+    repos <- getOption("repos")
+    reposBak <- repos
+    repos <- append(x = repos, values = BiocManager::repositories())
+    options("repos" = repos)
+    out <- tools::package_dependencies(
+        packages = package,
+        db = NULL,
+        which = c("Depends", "Imports", "LinkingTo"),
+        recursive = TRUE,
+        reverse = FALSE
+    )
+    out <- out[[1L]]
+    options("repos" = reposBak)
+    out
+}
