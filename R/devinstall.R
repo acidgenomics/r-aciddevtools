52b1821a18d4643c4f392dfdf9e467f20b5afdb8
Michael Steinbaugh <mike@steinbaugh.com>
2023-05-02 12:39:11 -0400

    Rework devinstall function

---
+++ R/devinstall.R
@@ -0,0 +1,27 @@
+#' Install package in development
+#'
+#' @export
+#'
+#' @param pkg `character(1)`.
+#' Package directory path.
+#'
+#' @param dependencies `logical(1)`.
+#' Also include dependencies.
+#' Use `TRUE` to also install all suggested packages.
+#'
+#' @return Invisible `character(1)`.
+#' Package directory path
+devinstall <- function(pkg = getwd(), dependencies = FALSE) {
+    stopifnot(
+        .requireNamespaces("devtools"),
+        isTRUE(dir.exists(pkg))
+    )
+    repos <- getOption("repos")
+    if (!isFALSE(dependencies)) {
+        stopifnot(.requireNamespaces("BiocManager"))
+        options("repos" = BiocManager::repositories())
+    }
+    devtools::install(pkg, dependencies = dependencies)
+    options("repos" = repos)
+    invisible(pkgs)
+}
