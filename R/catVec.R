541955915a148dbf2a2b7a739d68915913cf02b3
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-07 13:54:59 -0400

    Improve catVec to support named vectors

--- R/catVec.R
+++ R/catVec.R
@@ -1,13 +1,33 @@
-#' Print (cat) a vector
+#' Print (cat) a vector to console
 #'
 #' @export
-#' @note Updated 2019-08-13.
+#' @note Updated 2023-09-07.
 #'
 #' @inheritParams params
 #'
 #' @return Console output.
+#'
+#' @examples
+#' ## Unnamed vector.
+#' x <- c("aaa", "bbb", "ccc")
+#' catVec(x)
+#'
+#' ## Named vector.
+#' x <- c("aaa" = "AAA", "bbb" = "BBB", "ccc" = "CCC")
+#' catVec(x)
 catVec <- function(x) {
-    x <- paste0("\"", x, "\"")
+    stopifnot(is.character(x))
+    if (is.null(names(x))) {
+        x <- vapply(X = x, FUN = deparse, FUN.VALUE = character(1L))
+    } else {
+        x <- unlist(Map(
+            name = names(x),
+            value = unname(x),
+            f = function(name, value) {
+                paste0(deparse(name), " = ", deparse(value))
+            }
+        ))
+    }
     x <- paste(x, collapse = ",\n")
     cat(x)
 }
--- tests/testthat/test-catVec.R
+++ tests/testthat/test-catVec.R
@@ -1,6 +1,13 @@
-test_that("catVec", {
+test_that("Unnamed vector", {
     expect_output(
         object = catVec(c("aaa", "bbb")),
         regexp = "\"aaa\","
+    )
+})
+
+test_that("Named vector", {
+    expect_output(
+        object = catVec(c("aaa" = "AAA", "bbb" = "BBB")),
+        regexp = "\"aaa\" = \"AAA\","
     )
 })
