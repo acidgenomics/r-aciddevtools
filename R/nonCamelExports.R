f4a39676c3d0466ba006a1e56f373614a3162483
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-27 09:27:58 -0400

    Don't allow all capital classes

--- R/nonCamelExports.R
+++ R/nonCamelExports.R
@@ -35,7 +35,7 @@ nonCamelExports <- function(path = getwd()) {
     ## Ignore coercion methods (e.g. "as.DataFrame").
     x <- x[!grepl(pattern = "^as\\.", x = x)]
     ## Ignore all uppercase exports (e.g. "HGNC", "MGI").
-    x <- x[!grepl(pattern = "^[A-Z0-9]+$", x = x)]
+    ## > x <- x[!grepl(pattern = "^[A-Z0-9]+$", x = x)]
     x <- sort(unique(x))
     subx <- substr(x, start = 1L, stop = 1L)
     case <- list()
