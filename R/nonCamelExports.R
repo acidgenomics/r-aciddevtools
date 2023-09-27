87c9adf20c737b33780068930ccf046889f4f278
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-27 09:23:08 -0400

    Also check methods

--- R/nonCamelExports.R
+++ R/nonCamelExports.R
@@ -19,7 +19,7 @@ nonCamelExports <- function(path = getwd()) {
     file <- file.path(path, "NAMESPACE")
     stopifnot(.isAFile(file))
     x <- readLines(file)
-    x <- grep(pattern = "^export(Classes)?\\(", x = x, value = TRUE)
+    x <- grep(pattern = "^export(Classes|Methods)?\\(", x = x, value = TRUE)
     if (!.hasLength(x)) {
         return(character())
     }
