2f9ae75de2b033b3d7fb19716494c9a432616d5a
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 17:53:54 -0400

    Adjust spacing

--- R/nonCamelArgs.R
+++ R/nonCamelArgs.R
@@ -12,63 +12,62 @@
 #'
 #' @return `character`.
 #' Offending Rd documentation files.
-nonCamelArgs <- function(
-        pkgName,
-        allowlist = c("BPPARAM", "FUN", "MARGIN", "USE.NAMES", "X", "do.NULL")
-    ) {
-    stopifnot(
-        .requireNamespaces(c("syntactic", "tools")),
-        .isString(pkgName),
-        is.character(allowlist)
-    )
-    allowlist <- c(allowlist, "\n", "...")
-    db <- tools::Rd_db(pkgName)
-    lst <- lapply(
-        X = db,
-        allowlist = allowlist,
-        FUN = function(rd, allowlist) {
-            tag <- "arguments"
-            tags <- rdTags(rd)
-            if (!.isSubset(tag, tags)) {
-                return(NULL)
+nonCamelArgs <-
+    function(pkgName,
+             allowlist = c("BPPARAM", "FUN", "MARGIN", "USE.NAMES", "X", "do.NULL")) {
+        stopifnot(
+            .requireNamespaces(c("syntactic", "tools")),
+            .isString(pkgName),
+            is.character(allowlist)
+        )
+        allowlist <- c(allowlist, "\n", "...")
+        db <- tools::Rd_db(pkgName)
+        lst <- lapply(
+            X = db,
+            allowlist = allowlist,
+            FUN = function(rd, allowlist) {
+                tag <- "arguments"
+                tags <- rdTags(rd)
+                if (!.isSubset(tag, tags)) {
+                    return(NULL)
+                }
+                data <- rd[which(tags == tag)][[1L]]
+                vec <- vapply(
+                    X = data,
+                    FUN = function(x) {
+                        x[[1L]][[1L]]
+                    },
+                    FUN.VALUE = character(1L)
+                )
+                vec <- setdiff(vec, allowlist)
+                if (identical(vec, tolower(vec))) {
+                    return(NULL)
+                }
+                vec
             }
-            data <- rd[which(tags == tag)][[1L]]
-            vec <- vapply(
-                X = data,
-                FUN = function(x) {
-                    x[[1L]][[1L]]
-                },
-                FUN.VALUE = character(1L)
-            )
-            vec <- setdiff(vec, allowlist)
-            if (identical(vec, tolower(vec))) {
-                return(NULL)
-            }
-            vec
+        )
+        lst <- Filter(Negate(is.null), lst)
+        if (!.hasLength(lst)) {
+            return(character())
         }
-    )
-    lst <- Filter(Negate(is.null), lst)
-    if (!.hasLength(lst)) {
-        return(character())
+        lst <- lapply(
+            X = lst,
+            FUN = function(x) {
+                ## Split combined arguments, when necessary.
+                if (any(grepl(pattern = ", ", x = x))) {
+                    x <- unique(unlist(strsplit(x, split = ", ")))
+                }
+                ## Ignore leading "." on arguments, such as ".xname".
+                x <- sub(pattern = "^\\.", replacement = "", x = x)
+                y <- syntactic::camelCase(x)
+                x[x != y]
+            }
+        )
+        lst <- Filter(.hasLength, lst)
+        out <- paste(
+            names(lst),
+            lapply(X = lst, FUN = toString),
+            sep = ": "
+        )
+        out
     }
-    lst <- lapply(
-        X = lst,
-        FUN = function(x) {
-            ## Split combined arguments, when necessary.
-            if (any(grepl(pattern = ", ", x = x))) {
-                x <- unique(unlist(strsplit(x, split = ", ")))
-            }
-            ## Ignore leading "." on arguments, such as ".xname".
-            x <- sub(pattern = "^\\.", replacement = "", x = x)
-            y <- syntactic::camelCase(x)
-            x[x != y]
-        }
-    )
-    lst <- Filter(.hasLength, lst)
-    out <- paste(
-        names(lst),
-        lapply(X = lst, FUN = toString),
-        sep = ": "
-    )
-    out
-}
