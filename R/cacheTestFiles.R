c8a433ea88587b5e684d7dd4569ad5ce72e1df17
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-28 13:45:13 -0400

    Don't allow the user to change the remote bucket

--- R/cacheTestFiles.R
+++ R/cacheTestFiles.R
@@ -9,38 +9,40 @@
 #' @param files `character`.
 #' File basenames relative to `remoteDir`.
 #'
-#' @param remoteDir `character(1)`.
-#' Remote base URL.
-#'
 #' @return Invisible `character`.
 #' File paths.
-cacheTestFiles <- function(pkg, files, remoteDir) {
-    stopifnot(
-        .requireNamespaces(c("AcidBase", "goalie")),
-        goalie::hasInternet(),
-        goalie::isString(pkg),
-        goalie::isCharacter(files),
-        goalie::isAnExistingUrl(remoteDir)
-    )
-    cacheDir <- AcidBase::pkgCacheDir(.pkgName)
-    cacheDir <- file.path(cacheDir, "testthat")
-    cacheDir <- AcidBase::initDir(cacheDir)
-    out <- vapply(
-        X = files,
-        FUN = function(file, remoteDir) {
-            destfile <- file.path(cacheDir, file)
-            if (!goalie::isAFile(destfile)) {
-                AcidBase::download(
-                    url = AcidBase::pasteUrl(remoteDir, file),
-                    destfile = destfile
-                )
-            }
-            destfile
-        },
-        FUN.VALUE = character(1L),
-        remoteDir = remoteDir,
-        USE.NAMES = FALSE
-    )
-    stopifnot(goalie::allAreFiles(out))
-    invisible(out)
-}
+cacheTestFiles <-
+    function(pkg, files) {
+        stopifnot(
+            .requireNamespaces(c("AcidBase", "goalie")),
+            goalie::hasInternet(),
+            goalie::isString(pkg),
+            goalie::isCharacter(files)
+        )
+        remoteDir <- AcidBase::pasteURL(
+            "r.acidgenomics.com", "testdata", tolower(pkg),
+            protocol = "https"
+        )
+        stopifnot(goalie::isAnExistingUrl(remoteDir))
+        cacheDir <- AcidBase::pkgCacheDir(.pkgName)
+        cacheDir <- file.path(cacheDir, "testthat")
+        cacheDir <- AcidBase::initDir(cacheDir)
+        out <- vapply(
+            X = files,
+            FUN = function(file, remoteDir) {
+                destfile <- file.path(cacheDir, file)
+                if (!goalie::isAFile(destfile)) {
+                    AcidBase::download(
+                        url = AcidBase::pasteUrl(remoteDir, file),
+                        destfile = destfile
+                    )
+                }
+                destfile
+            },
+            FUN.VALUE = character(1L),
+            remoteDir = remoteDir,
+            USE.NAMES = FALSE
+        )
+        stopifnot(goalie::allAreFiles(out))
+        invisible(out)
+    }
