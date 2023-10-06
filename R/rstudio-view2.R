6d35b9c47f865963fa6ea170168aca525a5450e6
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-06 05:51:26 -0400

    Need to set optional here

--- R/rstudio-view2.R
+++ R/rstudio-view2.R
@@ -16,13 +16,18 @@
 #' This function puts up a window and returns immediately.
 #' In RStudio, this will open a tabbed window.
 #'
+#' @seealso
+#' - https://support.bioconductor.org/p/9137534/
+#'
 #' @examples
 #' ## > view2(mtcars)
 view2 <- function(object) {
     ## Coerce S4 object, if applicable.
     if (.isAny(object, c("DataFrame", "GenomicRanges"))) {
         ## Coercion with `as.data.frame` can change column names.
-        object <- as(object, "data.frame")
+        ## Alternative approach:
+        ## > object <- as(object, "data.frame")
+        object <- as.data.frame(object, optional = TRUE)
     }
     if (!.isRstudio()) {
         stopifnot(.requireNamespaces("utils"))
