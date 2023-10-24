d1855ff94e08ac6ca58c734d32ea517fbfffaff7
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-24 09:04:26 -0400

    Pass lints

--- R/rebuildBinary.R
+++ R/rebuildBinary.R
@@ -17,43 +17,44 @@
 #'
 #' @examples
 #' ## > rebuildBinary(c("AcidSingleCell", "DESeqAnalysis"))
-rebuildBinary <- function(packageName,
-                          repo = file.path("~", "monorepo", "r-acidgenomics-com")) {
-    stopifnot(
-        .isCharacter(packageName),
-        .isADir(repo)
-    )
-    invisible(lapply(
-        X = packageName,
-        repo = repo,
-        FUN = function(packageName, repo) {
-            contribDir <- file.path(repo, "src", "contrib")
-            stopifnot(.isADir(contribDir))
-            srcTarball <- sort(list.files(
-                path = contribDir,
-                pattern = packageName,
-                full.names = TRUE
-            ))
-            stopifnot(.isAFile(srcTarball))
-            command <- file.path(R.home(), "bin", "R")
-            tempdir <- .tempdir2()
-            message("Building binary tarball.")
-            .shell(
-                command = command,
-                args = c(
-                    "CMD", "INSTALL", "--build",
-                    srcTarball
-                ),
-                wd = tempdir
-            )
-            binTarball <- sort(list.files(
-                path = tempdir,
-                pattern = "*.tgz",
-                full.names = TRUE
-            ))
-            out <- .insertPackage(file = binTarball, repo = repo)
-            .unlink2(tempdir)
-            out
-        }
-    ))
-}
+rebuildBinary <-
+    function(packageName,
+             repo = file.path("~", "monorepo", "r-acidgenomics-com")) {
+        stopifnot(
+            .isCharacter(packageName),
+            .isADir(repo)
+        )
+        invisible(lapply(
+            X = packageName,
+            repo = repo,
+            FUN = function(packageName, repo) {
+                contribDir <- file.path(repo, "src", "contrib")
+                stopifnot(.isADir(contribDir))
+                srcTarball <- sort(list.files(
+                    path = contribDir,
+                    pattern = packageName,
+                    full.names = TRUE
+                ))
+                stopifnot(.isAFile(srcTarball))
+                command <- file.path(R.home(), "bin", "R")
+                tempdir <- .tempdir2()
+                message("Building binary tarball.")
+                .shell(
+                    command = command,
+                    args = c(
+                        "CMD", "INSTALL", "--build",
+                        srcTarball
+                    ),
+                    wd = tempdir
+                )
+                binTarball <- sort(list.files(
+                    path = tempdir,
+                    pattern = "*.tgz",
+                    full.names = TRUE
+                ))
+                out <- .insertPackage(file = binTarball, repo = repo)
+                .unlink2(tempdir)
+                out
+            }
+        ))
+    }
